3d2f0011c847a1d339aa95a4190b567672952a92
Towner Hale <townerhaleTowner@Towners-MacBook-Air.local>
2020-09-21 10:38:46 -1000

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
-export default{
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
     STRIPE_KEY: "pk_test_51HQgNQFXjxfmP3NeMHmSfzjhP02W7OtsZo7fFrJiqnGz1DreNVgruCPLsRshpyKhXhiK2A6vzkYb1NtqmzZJJldK00pwEvEt1x",
     s3: {
-        REGION: "us-east-1",
-        BUCKET: "town-notes-app"
+      REGION: "us-east-1",
+      BUCKET: "notes-app-2-api-dev-attachmentsbucket-5vcaywz9nvrx"
     },
     apiGateway: {
-        REGION: "us-east-1",
-        URL: "https://96k8vbkn79.execute-api.us-east-1.amazonaws.com/prod"
+      REGION: "us-east-1",
+      URL: "https://cn0ujdp9v0.execute-api.us-east-1.amazonaws.com/dev"
     },
     cognito: {
         REGION: "us-east-1",
-        USER_POOL_ID: "us-east-1_p0aV67lu0",
-        APP_CLIENT_ID: "4g621ltqejmk5oj9o6skrat835",
-        IDENTITY_POOL_ID: "us-east-1:64d9f280-2a6e-49bb-8b83-c007df432f9c"
+        USER_POOL_ID: "us-east-1_ACHxIaNMr",
+        APP_CLIENT_ID: "4itq199kcpuabrvmv687uvp6qn",
+        IDENTITY_POOL_ID: "us-east-1:29fc5f3f-42ed-4345-922f-32f29781045f"
+      }
+  };
+
+  const prod = {
+    STRIPE_KEY: "pk_test_51HQgNQFXjxfmP3NeMHmSfzjhP02W7OtsZo7fFrJiqnGz1DreNVgruCPLsRshpyKhXhiK2A6vzkYb1NtqmzZJJldK00pwEvEt1x",
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-2-api-prod-attachmentsbucket-1ixyfh9kxcfqj"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://wmxdkzk226.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_kN7z1MAHG",
+      APP_CLIENT_ID: "4rf0uoc3r9k9e1nbajk34anoi0",
+      IDENTITY_POOL_ID: "us-east-1:e8922dee-bb3a-4e73-98e3-0d19b730304d"
     }
-};
+  };
+
+  // Default to dev if not set
+  const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+  export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
+  };
